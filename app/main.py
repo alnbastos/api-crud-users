b180ec2ee090fe8ef61f53b55affce6e72703305
Aline Bastos <alinebastos23@outlook.com>
2023-07-03 23:43:47 -0300

    fix: remove read users

--- app/main.py
+++ app/main.py
@@ -18,14 +18,3 @@ def create_user(users: User):
     database.create(collection='users', data=user)
     return {'user': 'Registered user.'}
 
-@app.get('/users/')
-def read_user():
-    list_users = list()
-
-    for user in database.read_all('users'):
-        dict_user = user.to_dict()
-        dict_user['id'] = user.id
-
-        list_users.append(dict_user)
-
-    return {'user': list_users}
